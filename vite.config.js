99a1c12c82064df219d5e978e68088fc0666e364
Nayak, Rohan <rohannayak@Rohans-MBP-2.lan>
2025-01-14 20:00:00 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Set to your repository name
 })
